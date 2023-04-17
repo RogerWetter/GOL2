a2c60bc67d62469951501fd0103edd088901d4eb
RogerWetter <79643516+RogerWetter@users.noreply.github.com>
2023-04-17 10:00:59 +0200

    Initial Commit

---
+++ GOL2/GOL2App.swift
@@ -0,0 +1,17 @@
+//
+//  GOL2App.swift
+//  GOL2
+//
+//  Created by roger wetter on 17.04.23.
+//
+
+import SwiftUI
+
+@main
+struct GOL2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GOL2UITests/GOL2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GOL2UITestsLaunchTests.swift
+//  GOL2UITests
+//
+//  Created by roger wetter on 17.04.23.
+//
+
+import XCTest
+
+final class GOL2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
